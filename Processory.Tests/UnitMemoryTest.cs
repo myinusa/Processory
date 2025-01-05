ae2c6e5568f0c3adf902aec2892d21e45f40b982
Matthias Yinusa <49248934+myinusa@users.noreply.github.com>
2025-01-05 13:42:00 +0000

    fix(UnitMemoryTest.cs): Corrected logic for logging and validation

--- Processory.Tests/UnitMemoryTest.cs
+++ Processory.Tests/UnitMemoryTest.cs
@@ -1,7 +1,7 @@
 using Microsoft.Extensions.Logging;
 using Xunit.Abstractions;
 
-namespace Processory.Tests; 
+namespace Processory.Tests;
 public class MemoryReaderTests(ITestOutputHelper testOutputHelper) : CommonTestBase(testOutputHelper) {
     private readonly ProcessoryClient processoryClient = new("fm", new LoggerFactory());
     private const ulong BaseAddress = 0x140000000;
@@ -9,39 +9,27 @@ public class MemoryReaderTests(ITestOutputHelper testOutputHelper) : CommonTestB
 
 
     [Fact]
-    public void TestProcessIsValid()
-    {
-        // Arrange
-        const ulong validAddress = BaseAddress + GameExceptionHandlerOffset; // Use a known valid address
+    public void TestProcessIsValid() {
+        const ulong validAddress = BaseAddress + GameExceptionHandlerOffset;
+        var isValid = processoryClient.AddressHelper.IsValidAddress(validAddress);
 
-        // Act
-        var isValid = processoryClient.AddressHelper.IsValidAddress((IntPtr)validAddress);
+        Logger.LogInformation("Address: {X}", validAddress);
 
-        // Log
-        Logger.LogInformation("Address: {0:X}", validAddress);
-
-        // Assert
         Assert.True(isValid, "The address should be valid.");
     }
 
     [Fact]
     public void TestReadMemory() {
-        // Replace with a valid address and expected value
-        // ulong address = 0xYourMemoryAddress; // Replace with a valid memory address
 
         const ulong addr1 = BaseAddress + GameExceptionHandlerOffset;
         var address = processoryClient.MemoryReader.Read<ulong>(addr1);
 
-        const int expectedValue = 12345; // Replace with the expected value at that address
+        const int expectedValue = 12345;
 
-        // testOutputHelper.WriteLine("Address (BaseAddress + Offset): {0:X}", address);
+        Logger.LogInformation("Address (BaseAddress + Offset): {X}", address);
 
-        Logger.LogInformation("Address (BaseAddress + Offset): {0:X}", address);
-
-        // Act
         var actualValue = processoryClient.MemoryReader.Read<int>(address);
 
-        // Assert
         Assert.NotEqual(expectedValue, actualValue);
     }
 }
